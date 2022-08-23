1d2f334e980b998c71a616f0892e260187dcc1c3
EgorRybin <antoniopareiro@gmail.com>
2022-08-23 14:40:03 +0300

    update 08 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,25 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRef = document.querySelector(".gallery");
+
+const listItem = galleryItems
+  .map(
+    (img) => `<li>
+  <a class="gallery__item" href="${img.original}">
+    <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+  </a>
+</li>
+`
+  )
+  .join("");
+
+galleryRef.innerHTML = listItem;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
